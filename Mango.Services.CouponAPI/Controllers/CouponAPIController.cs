b9df16e290113b811ece0d9ac9d870a52cd60587
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2023-12-27 02:39:17 +0100

    created HttpGet and HttpGet By Id methods

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Migrations;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -7,6 +10,43 @@ namespace Mango.Services.CouponAPI.Controllers
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
+        private readonly AppDbContext _db;
 
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return null;
+        }
+
+
+        [HttpGet("{Id:int}")]
+        public object Get(int Id)
+        {
+            try
+            {
+               var objList = _db.Coupons.First(u => u.CouponId == Id);
+                return objList;
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return null;
+        }
     }
 }
